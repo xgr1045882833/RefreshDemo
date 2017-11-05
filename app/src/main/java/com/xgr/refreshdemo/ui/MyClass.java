0d7c1c696ca7c31ff215f6b01ce2d795a7c2a857
xieguorong <xie_guo_rong@163.com>
2017-11-05 17:58:49 +0800

    add user

--- app/src/main/java/com/xgr/refreshdemo/ui/MyClass.java
+++ app/src/main/java/com/xgr/refreshdemo/ui/MyClass.java
@@ -6,4 +6,38 @@ package com.xgr.refreshdemo.ui;
 
 public class MyClass {
     //哈哈哈
+    private String name;
+    private int age;
+
+    public MyClass() {
+    }
+
+    public MyClass(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "MyClass{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
