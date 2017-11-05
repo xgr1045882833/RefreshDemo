3351e0d0a650ab42f2af98dd70e57521c5126c45
xieguorong <xie_guo_rong@163.com>
2017-11-05 18:02:34 +0800

    第二次提交

--- app/src/main/java/com/xgr/refreshdemo/ui/MyClass.java
+++ app/src/main/java/com/xgr/refreshdemo/ui/MyClass.java
@@ -6,15 +6,11 @@ package com.xgr.refreshdemo.ui;
 
 public class MyClass {
     //哈哈哈
+
     private String name;
-    private int age;
 
-    public MyClass() {
-    }
-
-    public MyClass(String name, int age) {
+    public MyClass(String name) {
         this.name = name;
-        this.age = age;
     }
 
     public String getName() {
@@ -25,19 +21,4 @@ public class MyClass {
         this.name = name;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "MyClass{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
